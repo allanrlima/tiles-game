721600c5749e52abca06d6a918549c723ac8e34f
Allan Rodrigues Lima <allanr.lima@hotmail.com>
2019-01-18 09:10:00 -0200

    first commit

---
+++ src/components/Tile/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const TileWrapper = styled.div`
+  background-color: red;
+`;
+
+export const Tile = () => {
+  return (
+    <TileWrapper>
+      <div>test</div>
+    </TileWrapper>
+  );
+};
